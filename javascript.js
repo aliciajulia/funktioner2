dff63dd9520a083ab896d01e63ab3ca53f7a3417
alibro916 <alibro916@E29778.edu.linkoping.se>
2015-03-06 14:55:38 +0100

    inlogskiten

--- javascript.js
+++ javascript.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-//    console.log("hej");
-    $(".kasta").click(function() {
-        var x = Math.floor((Math.random() * 6) + 1);
-       var n = x.toString();
-        $('<p>').text(n).appendTo("#box1");
-        console.log(n);
-   
+
+    $('form').submit(function(event) {
+        event.preventDefault();
     });
+
+    function kasta($kastad) {
+
+        var tal = Math.floor((Math.random() * 6) + 1);
+        var talToString = tal.toString();
+        $('<p>').text(talToString).appendTo("#box");
+        console.log(talToString);
+    }
+    $(".kasta").click(function() {
+        kasta();
+    });
+
+    var antal = $('.antal:checked').val();
+    console.log(antal);
 });
-
-
-
